550dd743db59e915ef5ebaa32039a02e8c539e45
jfang <im.jonathan.fang@gmail.com>
2025-10-03 13:28:30 -0700

    save work on astro content collection that doesn't affect main tutorial

---
+++ src/content.config.ts
@@ -0,0 +1,22 @@
+// Import the glob loader
+import { glob } from "astro/loaders";
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `loader` and `schema` for each collection
+const blog = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+        description: z.string().optional(),
+        author: z.string().default("Jonathan Fang"),
+        image: z.object({
+            url: z.string(),
+            alt: z.string(),
+        }).optional(),
+        tags: z.array(z.string()).optional(),
+        // heroImage lol
+    }),
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = { blog };
