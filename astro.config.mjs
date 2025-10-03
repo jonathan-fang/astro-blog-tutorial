7a163453c8939377e9f66df7bad78418b7fd6549
jfang <im.jonathan.fang@gmail.com>
2025-10-03 12:07:35 -0700

    6.1 astro island preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 // export default defineConfig({});
 
@@ -14,4 +16,6 @@ export default defineConfig({
       },
     },
   },
+
+  integrations: [preact()],
 });
